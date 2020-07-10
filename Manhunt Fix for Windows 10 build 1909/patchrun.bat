05a825c4eb2a0bb499b72238a2c74bdd3b60ff21
gimalaji_blake <fjodor.kortsagin@gmail.com>
2020-07-10 12:33:04 +0300

    Add files via upload

---
+++ Manhunt Fix for Windows 10 build 1909/patchrun.bat
@@ -0,0 +1,7 @@
+@echo off
+title Manhunt on Windows Vista and Windows 7 Patcher
+link.exe -edit -nxcompat:no -dynamicbase:no manhunt.exe
+cls
+echo Manhunt should be patched now if you have installed this patch to the right directory.
+pause
+exit
